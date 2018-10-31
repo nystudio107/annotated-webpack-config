4ad9cc25f160684a2a2ec0090f03560580775928
Andrew Welch <andrew@nystudio107.com>
2018-10-31 15:00:31 -0400

    Coerce dotenv settings to Boolean
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

--- webpack.settings.js
+++ webpack.settings.js
@@ -61,9 +61,9 @@ module.exports = {
     devServerConfig: {
         public: () => process.env.DEVSERVER_PUBLIC || "http://localhost:8080",
         host: () => process.env.DEVSERVER_HOST || "localhost",
-        poll: () => process.env.DEVSERVER_POLL || false,
+        poll: () => Boolean(process.env.DEVSERVER_POLL) || false,
         port: () => process.env.DEVSERVER_PORT || 8080,
-        https: () => process.env.DEVSERVER_HTTPS || false,
+        https: () => Boolean(process.env.DEVSERVER_HTTPS) || false,
     },
     manifestConfig: {
         basePath: ""
