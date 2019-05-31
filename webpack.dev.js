127ff7be3dc1a4e9b0a1ea6b63cc32c75324cf40
Andrew Welch <andrew@nystudio107.com>
2019-05-31 13:29:12 -0400

    Removed `sane` since `webpack-dev-server ^3.3.0` fixes the sub-directory [watchContentBase issue](https://github.com/webpack/webpack-dev-server/issues/1694)
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

--- webpack.dev.js
+++ webpack.dev.js
@@ -5,7 +5,6 @@ const MODERN_CONFIG = 'modern';
 // node modules
 const merge = require('webpack-merge');
 const path = require('path');
-const sane = require('sane');
 const webpack = require('webpack');
 
 // webpack plugins
@@ -25,28 +24,15 @@ const configureDevServer = (buildType) => {
         port: settings.devServerConfig.port(),
         https: !!parseInt(settings.devServerConfig.https()),
         disableHostCheck: true,
-        quiet: true,
         hot: true,
-        hotOnly: true,
         overlay: true,
-        stats: 'errors-only',
+        watchContentBase: true,
         watchOptions: {
             poll: !!parseInt(settings.devServerConfig.poll()),
             ignored: /node_modules/,
         },
         headers: {
             'Access-Control-Allow-Origin': '*'
-        },
-        // Use sane to monitor all of the templates files and sub-directories
-        before: (app, server) => {
-            const watcher = sane(path.join(__dirname, settings.paths.templates), {
-                glob: ['**/*'],
-                poll: !!parseInt(settings.devServerConfig.poll()),
-            });
-            watcher.on('change', function(filePath, root, stat) {
-                console.log('  File modified:', filePath);
-                server.sockWrite(server.sockets, "content-changed");
-            });
         },
     };
 };
