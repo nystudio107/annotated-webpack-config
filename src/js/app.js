52d36f57d337893a83cd60a9bbabea05a99576d2
Andrew Welch <andrew@nystudio107.com>
2019-06-03 09:01:19 -0400

    Use destructuring for ESM module imports
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

--- src/js/app.js
+++ src/js/app.js
@@ -5,9 +5,9 @@ const main = async () => {
     // Import our CSS
     //const Styles = await import(/* webpackChunkName: "styles" */ '../css/app.pcss');
     // Async load the vue module
-    const Vue = await import(/* webpackChunkName: "vue" */ 'vue');
+    const { default: Vue } = await import(/* webpackChunkName: "vue" */ 'vue');
     // Create our vue instance
-    const vm = new Vue.default({
+    const vm = new Vue({
         el: "#app",
         components: {
             'confetti': () => import(/* webpackChunkName: "confetti" */ '../vue/Confetti.vue'),
