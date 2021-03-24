557101499ab8bbfaee142d6e3e29530a18f1d1e1
Andrew Welch <andrew@keluli.local>
2021-03-24 11:34:01 -0400

    Fixed an issue with the `webpack-dev-server` version `^4.0.0-beta.1` by moving the `overlay` config setting to `client`

--- buildchain/webpack-configs/dev-server.config.js
+++ buildchain/webpack-configs/dev-server.config.js
@@ -13,6 +13,7 @@ module.exports = (type = 'modern', settings) => {
     const common = () => ({
         devServer: {
             client: {
+                overlay: true,
                 progress: false,
             },
             dev: {
@@ -25,7 +26,6 @@ module.exports = (type = 'modern', settings) => {
             host: settings.host(),
             hot: true,
             https: !!parseInt(settings.https()),
-            overlay: true,
             port: settings.port(),
             public: settings.public(),
             static: {
